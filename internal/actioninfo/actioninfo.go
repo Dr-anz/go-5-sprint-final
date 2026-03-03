81f7bc645b5d8e0bf1446fbd5b9f40cb77e0812a
Anzor Kabaloev <dr-anz@mail.ru>
2026-03-03 14:51:31 +0300

    first commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,30 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(datastring string) (err error)
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	for _, data := range dataset {
+		// перебираем все значения слайса dataset в цикле
+		err := dp.Parse(data) // распарсить значение с помощью метода Parse()
+		if err != nil {       // обработать ошибку и залогировать её
+			log.Println(err)
+			continue
+		}
+		// сформировать строку с информацией об активности
+		info, err := dp.ActionInfo()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		// вывести информацию об активности
+		fmt.Println(info)
+	}
 }
--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,54 @@
 package daysteps
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	Personal personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	// разделяем строку на части
+	parts := strings.Split(datastring, ",")
+	// проверяем что частей ровно 2
+	if len(parts) != 2 {
+		return err
+	}
+	// парсим количество шагов
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return err
+	}
+	ds.Steps = steps
+	// парсинг длительности прогулки
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return err
+	}
+	ds.Duration = duration
+
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	// вычисляем дистанцию
+	distance := spentenergy.Distance(ds.Steps, ds.Personal.Height)
+	// расчитываем количество сожженых калорий
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Personal.Weight, ds.Personal.Height, ds.Duration)
+	if err != nil {
+		return "", err
+	}
+	// формирует итоговою строку
+	result := fmt.Sprintf("Количество шагов: %d.\nДистанция составила: %.2f км.\nВы сожгли: %.2f ккал.\n", ds.Steps, distance, calories)
+
+	return result, nil
 }
